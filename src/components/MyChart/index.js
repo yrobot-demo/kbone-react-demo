a4904bf951ce4065070bb5ef17a45ae489a6c2cd
Yrobot <Yrobot@qq.com>
2020-05-16 20:37:50 +0800

    1修复F2展示不高清问题，2实现canvas事件对接F2

--- src/components/MyChart/index.js
+++ src/components/MyChart/index.js
@@ -2,9 +2,6 @@ import F2 from '@antv/f2'
 import React, { useEffect, useRef, useState } from 'react'
 import './index.less'
 
-const Gesture = require('@antv/f2/lib/plugin/gesture')
-F2.Chart.plugins.register(Gesture)
-
 function wrapEvent(e) {
   if (!e) return
   if (!e.preventDefault) {
@@ -13,26 +10,15 @@ function wrapEvent(e) {
   return e
 }
 
-const suitCanvasEvent = canvas => {
-  // canvas.addEventListener('canvastouchstart', e => {
-  //   console.log('canvastouchstart')
-  //   canvas.dispatchEvent('mousedown', wrapEvent(e))
-  // })
-  // canvas.addEventListener('canvastouchmove', e => {
-  //   console.log('canvastouchmove')
-  //   canvas.dispatchEvent('mousemove', wrapEvent(e))
-  // })
-  // canvas.addEventListener('canvastouchend', e => {
-  //   console.log('canvastouchend')
-  //   canvas.dispatchEvent('mouseup', wrapEvent(e))
-  // })
-  canvas.addEventListener('canvasError', e => {
-    console.log('canvasError')
-    console.error(e)
+const suitCanvasEvent = (canvas, el) => {
+  canvas.addEventListener('canvastouchstart', e => {
+    el.dispatchEvent('touchstart', wrapEvent(e))
   })
-  canvas.addEventListener('canvaserror', e => {
-    console.log('canvaserror')
-    console.error(e)
+  canvas.addEventListener('canvastouchmove', e => {
+    el.dispatchEvent('touchmove', wrapEvent(e))
+  })
+  canvas.addEventListener('canvastouchend', e => {
+    el.dispatchEvent('touchend', wrapEvent(e))
   })
 }
 
@@ -47,49 +33,38 @@ const MyChart = ({
   const width = (screenWidth * 375) / 375
   const height = (screenWidth * 250) / 375
 
-  const myCanvas = useRef({})
   useEffect(() => {
-    if (myCanvas.current && !loading) {
+    if (!loading) {
       document
         .getElementById(id)
         .$$prepare()
         .then(domNode => {
-          myCanvas.current.$$prepare().then(canvas => {
-            // const ctx = domNode.getContext('2d')
-            // ctx.scale(pixelRatio, pixelRatio)
-            // const { pixelRatio } = wx.getSystemInfoSync()
-            F2.Global.pixelRatio = 1
-            F2.Global.fontFamily = 'sans-serif'
-            const chart = new F2.Chart({
-              id: id,
-              width: width,
-              height: height
-              // ...F2Config
-            })
-            doChart(chart)
-            if (chart) {
-              suitCanvasEvent(chart.get('el'))
-            }
+          domNode.width = width * pixelRatio
+          domNode.height = height * pixelRatio
+          const context = domNode.getContext('2d')
+          context.scale(pixelRatio, pixelRatio)
+
+          F2.Global.pixelRatio = 1
+          F2.Global.fontFamily = 'sans-serif'
+          const chart = new F2.Chart({
+            context,
+            width: width,
+            height: height
+            // ...F2Config
           })
+          doChart(chart)
+          suitCanvasEvent(domNode, chart.get('el'))
         })
     }
-  }, [myCanvas.current, loading])
+  }, [loading])
 
   return (
     <div className="my-chart">
       <canvas
         type="2d"
         style={{ width: width + 'px', height: height + 'px' }}
-        width={width}
-        height={height}
-        // width={width * pixelRatio}
-        // height={height * pixelRatio}
         id={id}
         canvas-id={id}
-        ref={myCanvas}
-        onError={e => {
-          console.error(e)
-        }}
       />
     </div>
   )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,30 +4,30 @@ import ReactDOM from 'react-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import createPage from '@/utils/pageBase'
 
-const doChart = (chart) => {
+const doChart = chart => {
   const data = [
     { shop: '家乐福-VS-成都大世界316', name: '货架日均客流', value: 517 },
     {
       shop: '家乐福-VS-成都大世界316',
       name: '七天前货架日均客流',
-      value: 1116,
+      value: 1116
     },
     { shop: '家乐福-VS-昆明白云店318', name: '货架日均客流', value: 459 },
     {
       shop: '家乐福-VS-昆明白云店318',
       name: '七天前货架日均客流',
-      value: 733,
+      value: 733
     },
     { shop: '家乐福-VS-深圳新洲店222', name: '货架日均客流', value: 257 },
     {
       shop: '家乐福-VS-深圳新洲店222',
       name: '七天前货架日均客流',
-      value: 1068,
+      value: 1068
     },
     { shop: '大润发-PNT-苏州东环店', name: '货架日均客流', value: 244 },
     { shop: '大润发-PNT-苏州东环店', name: '七天前货架日均客流', value: 565 },
     { shop: '大润发-PNT-上海大宁店', name: '货架日均客流', value: 230 },
-    { shop: '大润发-PNT-上海大宁店', name: '七天前货架日均客流', value: 651 },
+    { shop: '大润发-PNT-上海大宁店', name: '七天前货架日均客流', value: 651 }
   ]
   chart.source(data)
   chart.scale('shop', {
@@ -35,10 +35,10 @@ const doChart = (chart) => {
       return text.length > 10
         ? '...' + text.substr(text.length - 10, text.length)
         : text
-    },
+    }
   })
   chart.scale('value', {
-    formatter: (v) => parseFloat(v),
+    formatter: v => parseFloat(v)
   })
   chart.axis('value', {
     label: function label(text, index, total) {
@@ -49,41 +49,32 @@ const doChart = (chart) => {
         textCfg.textAlign = 'right'
       }
       return textCfg
-    },
+    }
   })
   chart.coord({
-    transposed: true,
+    transposed: true
   })
 
-  chart.pluginGesture({
-    gesture: {
-      touchstart(data, event) {
-        console.log('touchstart')
-      },
-      press(data, event) {
-        console.log('press')
-      },
-    },
-    hammerOptions: {},
-    options: {},
-  })
-
-  chart.interval().position('shop*value').color('name').adjust({
-    type: 'dodge',
-    marginRatio: 0.05, // 设置分组间柱子的间距
-  })
+  chart
+    .interval()
+    .position('shop*value')
+    .color('name')
+    .adjust({
+      type: 'dodge',
+      marginRatio: 0.05 // 设置分组间柱子的间距
+    })
   chart.render()
 }
 
 const Page = () => {
-  const todos = useSelector((state) => state.todos) || []
+  const todos = useSelector(state => state.todos) || []
   return (
     <div>
       <div>todos:</div>
-      {todos.map((v) => (
+      {todos.map(v => (
         <div
           style={{
-            marginTop: '10rpx',
+            marginTop: '10rpx'
           }}
         >
           {v}
@@ -91,7 +82,7 @@ const Page = () => {
       ))}
       <div
         style={{
-          marginTop: '100rpx',
+          marginTop: '100rpx'
         }}
         onClick={() => {
           window.navigateTo('/ShowUrlPage')
@@ -99,11 +90,7 @@ const Page = () => {
       >
         to addPage
       </div>
-      <MyChart
-        doChart={doChart}
-        id={'datagraphscflowest'}
-        loading={false}
-      />
+      <MyChart doChart={doChart} id={'datagraphscflowest'} loading={false} />
     </div>
   )
 }
