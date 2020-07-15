38fd12a81ef7e8f04cce1fe9512db9d658589e8a
yrobot <yrobotyang@whale.im>
2020-07-15 18:11:23 +0800

    单纯化graphql-server，直接使用graphql配合graphql-tools

--- cloudfunctions/graphql/index.js
+++ cloudfunctions/graphql/index.js
@@ -1,16 +1,24 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-const { graphqlWXServer } = require('wx-server-graphql')
+// const { graphqlWXServer } = require('wx-server-graphql')
 const { mergeSchema } = require('./utils/graphql')
+const { graphql } = require('graphql')
 
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-  return await graphqlWXServer({
-    wxParams: event,
-    context: wxContext,
-    schema: mergeSchema
+  return await graphql({
+    schema: mergeSchema,
+    source: event.query,
+    variableValues: event.variables,
+    operationName: event.operationName,
+    contextValue: wxContext
   })
+  // return await graphqlWXServer({
+  //   wxParams: event,
+  //   context: wxContext,
+  //   schema: mergeSchema
+  // })
 }
--- cloudfunctions/graphql/src/person/person.js
+++ cloudfunctions/graphql/src/person/person.js
@@ -1,6 +1,7 @@
 module.exports = {
   Query: {
     getPerson: (root, args, context) => {
+      console.log({root, args, context})
       const { firstName, lastName } = args
       return { name: `${firstName} ${lastName}`, age: 24 }
     }
