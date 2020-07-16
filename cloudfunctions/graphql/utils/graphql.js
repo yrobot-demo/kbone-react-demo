52e8ad321d7f6d7fca800f33038f8df0d9ee67fd
yrobot <yrobotyang@whale.im>
2020-07-16 16:49:01 +0800

    graphql-server的graphql部分添加错误try-catch

--- cloudfunctions/graphql/utils/graphql.js
+++ cloudfunctions/graphql/utils/graphql.js
@@ -1,27 +1,30 @@
 const { loadFilesSync } = require('@graphql-tools/load-files')
 const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
-const fs = require('fs')
 const path = require('path')
 
 const getTypeDefs = () => {
-  const gqlFiles = loadFilesSync(path.join(__dirname, '../src'), {
-    extensions: ['gql', 'graphql']
-  })
-  const typeDefs = mergeTypeDefs(gqlFiles, { all: true })
-  return typeDefs
+	const gqlFiles = loadFilesSync(path.join(__dirname, '../src'), {
+		extensions: ['gql', 'graphql']
+	})
+	const typeDefs = mergeTypeDefs(gqlFiles, { all: true })
+	return typeDefs
 }
 
 const getResolvers = () => {
-  const resolverFiles = loadFilesSync(path.join(__dirname, '../src'), {
-    extensions: ['js']
-  })
-  return mergeResolvers(resolverFiles)
+	const resolverFiles = loadFilesSync(path.join(__dirname, '../src'), {
+		extensions: ['js']
+	})
+	return mergeResolvers(resolverFiles)
 }
 
-module.exports = {
-  mergeSchema: makeExecutableSchema({
-    typeDefs: getTypeDefs(),
-    resolvers: getResolvers()
-  })
+try {
+	module.exports = {
+		mergeSchema: makeExecutableSchema({
+			typeDefs: getTypeDefs(),
+			resolvers: getResolvers()
+		})
+	}
+} catch (error) {
+	console.error(error)
 }
